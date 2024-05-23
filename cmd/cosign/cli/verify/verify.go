6de7982beffba1fca70f6a7c92e2598b1410da02
Jussi Kukkonen <jkukkonen@google.com>
2024-05-23 11:37:21 +0300

    add debug prints around sig.payload

--- cmd/cosign/cli/verify/verify.go
+++ cmd/cosign/cli/verify/verify.go
@@ -374,11 +374,13 @@ func PrintVerification(ctx context.Context, verified []oci.Signature, output str
 				}
 			}
 
+			ui.Infof(ctx, "sig.payload()...")
 			p, err := sig.Payload()
 			if err != nil {
 				fmt.Fprintf(os.Stderr, "Error fetching payload: %v", err)
 				return
 			}
+			ui.Infof(ctx, "sig.payload() ok")
 			fmt.Println(string(p))
 		}
 
